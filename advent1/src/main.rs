52ac1f46db91a149d6c15d310e3b7e1b7259cb7c
noxonad <noxonad@proton.me>
2024-12-02 15:07:31 +0200

    star one part two

--- advent1/src/main.rs
+++ advent1/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashMap};
 use std::fs;
 
 fn get_distance(input_string: String) -> u32 {
@@ -34,10 +34,36 @@ fn get_distance(input_string: String) -> u32 {
     distance
 }
 
+fn get_similarity(input_string: String) -> u32 {
+    let input_lines = input_string.lines();
+
+    let mut lmap: HashMap<u32, u8> = HashMap::new();
+    let mut rmap: HashMap<u32, u8> = HashMap::new();
+
+    // Map the numbers to their count in the input
+    for line in input_lines {
+        let mut s = line.split_whitespace();
+
+        let lnum: u32 = s.next().unwrap().parse().unwrap();
+        let rnum: u32 = s.next().unwrap().parse().unwrap();
+
+        lmap.entry(lnum).and_modify(|e| *e += 1).or_insert(1);
+        rmap.entry(rnum).and_modify(|e| *e += 1).or_insert(1);
+    }
+
+    let mut similarity: u32 = 0;
+    for (k, v) in lmap.iter() {
+        similarity += k * (*v as u32) * (*rmap.get(k).unwrap_or(&0) as u32);
+    }
+
+    similarity
+}
+
 fn main() {
     // Read from input file
     let input = fs::read_to_string("src/input.txt").expect("Could not read the puzzle input");
-    println!("distance: {}", get_distance(input));
+    println!("distance: {}", get_distance(input.clone()));
+    println!("similarity: {}", get_similarity(input.clone()));
 }
 
 #[cfg(test)]
@@ -45,10 +71,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn given_test() {
+    fn given_test_part_one() {
         let input =
             fs::read_to_string("src/test_input.txt").expect("Could not read the puzzle input");
         let e = 11;
         assert_eq!(e, get_distance(input))
+    }
+
+    #[test]
+    fn given_test_part_two() {
+        let input: String =
+            fs::read_to_string("src/test_input.txt").expect("Could not read the puzzle input");
+        let e: u32 = 31;
+        assert_eq!(e, get_similarity(input))
     }
 }
